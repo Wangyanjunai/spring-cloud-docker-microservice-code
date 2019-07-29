1083e7826d35eeb6c0bbc9de806ee343c3d3178a
wangyanjun <406151651@qq.com>
2019-07-29 12:01:06 +0800

    修改项目包配置和升级spring-boot，spring-cloud版本。

--- microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/study/user/controller/MovieController.java
+++ microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/study/user/controller/MovieController.java
@@ -1,6 +1,6 @@
-package com.itmuch.cloud.study.controller;
+package com.itmuch.cloud.study.user.controller;
 
-import com.itmuch.cloud.study.entity.User;
+import com.itmuch.cloud.study.user.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,6 +8,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * <pre>
+ * @PackageName com.itmuch.cloud.study.user.controller
+ * @ClassName MovieController
+ * @Desc
+ * @WebSite https://www.potato369.com
+ * @Author Jack
+ * @Date 2019/7/29 11:58
+ * @CreateBy IntellJ IDEA 2019.1.1
+ * @Copyright Copyright (c) 2016 ~ 2020 版权所有 (C) 土豆互联科技(深圳)有限公司 https://www.potato369.com All Rights Reserved。
+ * </pre>
+ */
 @RestController
 @RequestMapping("/movies")
 public class MovieController {
--- microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/study/user/entity/User.java
+++ microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/study/user/entity/User.java
@@ -1,4 +1,17 @@
-package com.itmuch.cloud.study.entity;
+package com.itmuch.cloud.study.user.entity;
+
+/**
+ * <pre>
+ * @PackageName com.itmuch.cloud.study.user.entity
+ * @ClassName User
+ * @Desc
+ * @WebSite https://www.potato369.com
+ * @Author Jack
+ * @Date 2019/7/29 11:55
+ * @CreateBy IntellJ IDEA 2019.1.1
+ * @Copyright Copyright (c) 2016 ~ 2020 版权所有 (C) 土豆互联科技(深圳)有限公司 https://www.potato369.com All Rights Reserved。
+ * </pre>
+ */
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
