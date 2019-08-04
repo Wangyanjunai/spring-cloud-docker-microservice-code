32035d2cb4d65e57426626724fdef3d41005b222
wangyanjun <406151651@qq.com>
2019-08-04 21:48:23 +0800

    添加通用方式使用Hystrix项目代码。

--- microservice-consumer-movie-ribbon-hystrix/src/main/java/com/itmuch/cloud/study/controller/MovieController.java
+++ microservice-consumer-movie-ribbon-hystrix/src/main/java/com/itmuch/cloud/study/controller/MovieController.java
@@ -1,6 +1,5 @@
 package com.itmuch.cloud.study.controller;
 
-
 import com.itmuch.cloud.study.entity.User;
 import com.itmuch.cloud.study.feign.UserFeignClient;
 
@@ -9,14 +8,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
-@RequestMapping("/movies")
+@RequestMapping(value = "/movies")
 public class MovieController {
 
     @Autowired
     private UserFeignClient userFeignClient;
 
-    @GetMapping("/users/{id}")
+    @GetMapping(value = "/users/{id}")
     public User findById(@PathVariable Long id) {
         // 这里用到了RestTemplate的占位符能力
     	return this.userFeignClient.findById(id);
