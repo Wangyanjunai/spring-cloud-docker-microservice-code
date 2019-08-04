bd53d7719e59fae86e1be230badfe7606ae23ab3
wangyanjun <406151651@qq.com>
2019-08-04 21:48:52 +0800

    添加通用方式使用Hystrix项目代码。

---
+++ microservice-consumer-movie-ribbon-hystrix-common/src/main/java/com/itmuch/cloud/study/controller/MovieController.java
@@ -0,0 +1,34 @@
+package com.itmuch.cloud.study.controller;
+
+
+import com.itmuch.cloud.study.entity.User;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @HystrixCommand(fallbackMethod = "findByIdFallback")
+    @GetMapping("/users/{id}")
+    public User findById(@PathVariable Long id) {
+        // 这里用到了RestTemplate的占位符能力
+    	return this.restTemplate.getForObject("http://localhost:8000/users/{id}", User.class, id);
+        // ......电影相关业务逻逻辑
+    }
+    
+    public User findByIdFallback(Long id) {
+		return new User(id, "accountDefault", "默认用户", 0, new BigDecimal(1));
+	}
+}
