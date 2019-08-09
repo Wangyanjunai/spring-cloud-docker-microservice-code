eef31d75ec3f83e7144691cc8d24e38c69a7e3f2
wangyanjun <406151651@qq.com>
2019-08-10 01:50:23 +0800

    修改项目代码

--- microservice-config-client-authenticating/src/main/java/com/itmuch/cloud/study/ConfigClientApplication.java
+++ microservice-config-client-authenticating/src/main/java/com/itmuch/cloud/study/ConfigClientApplication.java
@@ -1,18 +1,13 @@
 package com.itmuch.cloud.study;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ConfigClientApplication {
-	
-	private static final Logger LOGGER = LoggerFactory.getLogger(ConfigClientApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigClientApplication.class, args);
-		LOGGER.info("The microsoft service config client application has bean started...");
 	}
 
 }
--- microservice-config-server/src/main/java/com/itmuch/cloud/study/ConfigServerApplication.java
+++ microservice-config-server/src/main/java/com/itmuch/cloud/study/ConfigServerApplication.java
@@ -1,5 +1,7 @@
 package com.itmuch.cloud.study;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -8,8 +10,11 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 public class ConfigServerApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(ConfigServerApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
+		LOGGER.info("The microsoft service config server application has bean started...");
 	}
 
 }
