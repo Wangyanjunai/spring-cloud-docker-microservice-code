5363bee6fce6f2d2ce6d444bd8cdff61164ac384
wangyanjun <406151651@qq.com>
2019-08-10 22:02:59 +0800

    修改配置

--- microservice-simple-consumer-movie-trace/src/main/java/com/itmuch/cloud/study/controller/MovieController.java
+++ microservice-simple-consumer-movie-trace/src/main/java/com/itmuch/cloud/study/controller/MovieController.java
@@ -2,6 +2,7 @@ package com.itmuch.cloud.study.controller;
 
 import com.itmuch.cloud.study.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 public class MovieController {
 
     @Autowired
+    @LoadBalanced
     private RestTemplate restTemplate;
 
     @GetMapping("/users/{id}")
