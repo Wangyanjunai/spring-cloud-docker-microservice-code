ebdfe7a2b1b730f499f59f2ca3e72566de282c91
wangyanjun <406151651@qq.com>
2019-08-12 09:59:40 +0800

    修改代码

--- microservice-discovery-eureka-authenticating/src/main/java/com/itmuch/cloud/study/DiscoveryEurekaApplication.java
+++ microservice-discovery-eureka-authenticating/src/main/java/com/itmuch/cloud/study/DiscoveryEurekaApplication.java
@@ -1,5 +1,7 @@
 package com.itmuch.cloud.study;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -8,8 +10,11 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class DiscoveryEurekaApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(DiscoveryEurekaApplication.class);
+	
     public static void main(String[] args) {
         SpringApplication.run(DiscoveryEurekaApplication.class, args);
+        LOGGER.info("The microsoft service discovery eureka application has bean started...");
     }
 
 }
